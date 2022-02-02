974b443f9aa77ceeaf4039523d177ed8632a8333
Kayque <lkcoelho76@gmail.com>
2022-02-02 08:55:41 -0300

    feat: create endpoint to registrate new users into database

--- src/app.js
+++ src/app.js
@@ -1,12 +1,54 @@
 import express, { json } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { MongoClient } from "mongodb"; 
+import joi from "joi";
+import bcrypt from "bcrypt";
 
 dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+mongoClient.connect(() => {
+  db = mongoClient.db("my-wallet")
+})
+
+const userSchema = joi.object({
+  name: joi.string().required(),
+  email: joi.string().email().required(),
+  password: joi.string().required()
+})
 
 const app = express();
 app.use(json());
 app.use(cors());
+
+app.post("/sign-up", async (req, res) => {
+  const user = req.body;
+  const validation = userSchema.validate(user, { abortEarly: false });
+
+  if (validation.error) {
+    const arrErrors = validation.error.details.map((e) => e.message);
+    const message = arrErrors.join(", ");
+    return res.status(422).send(message);
+  }
+
+  try {
+    const isRegistrated = await db.collection("users").findOne({ email: user.email });
+
+    if (isRegistrated){
+      return res.sendStatus(409);
+    }
+
+    const passwordHash = bcrypt.hashSync(user.password, 10);
+    await db.collection("users").insertOne({ ...user, password: passwordHash });
+    res.sendStatus(201);
+    
+  } catch(error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
 
 app.listen(5000, () => {
   console.log("Server is listening on port 5000.")
